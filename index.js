9719c299c992c592b5adceacd2fff4eaf27e53ff
mahmudulhasan-xs <36738052+mahmudsagar@users.noreply.github.com>
2023-10-10 12:39:12 +0600

    Create api-schema

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cors = require('cors'); // Import the cors package
 
 const app = express();
-const port = 8013;
+const port = 8014;
 
 // Use the cors middleware with the allowed origin
 app.use(cors({
@@ -23,7 +23,9 @@ app.get('/api/:folder', (req, res) => {
   const folder = req.params.folder;
   const filePath = path.join(__dirname, '/components/',folder,`data.jsonc` );
   const data = fs.readFileSync(filePath, 'utf8')
-  res.status(200).send(data);
+  const cleanedJSONString = data.replace(/\/\/.*|\/\*[\s\S]*?\*\//g, '');
+  const cleanedJSONObject = JSON.parse(cleanedJSONString)
+  res.status(200).send(cleanedJSONObject);
 });
 
 // Define a route to serve JSON files based on the API endpoint
@@ -35,8 +37,11 @@ app.get('/api/:folder/:file', (req, res) => {
   const filePath = path.join(__dirname, '/components/',folder,`${file}.jsonc` );
   let data = fs.readFileSync(filePath, 'utf8')
   // data = JSON.parse(data);
+  const cleanedJSONString = data.replace(/\/\/.*|\/\*[\s\S]*?\*\//g, '');
+  const cleanedJSONObject = JSON.parse(cleanedJSONString)
+  // console.log(cleanedJSONObject);
   //res send with 200 status code
-  res.status(200).send(data);
+  res.status(200).send(cleanedJSONObject);
 
 
   // console.log(data);
